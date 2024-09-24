cf094f09b0ee873f6eaceaf6f5eb42e39d92e5e4
Cactus137 <Anhdo13072004@gmail.com>
2024-09-24 11:02:43 +0700

    first cm

---
+++ app/Models/CartItemTopping.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItemTopping extends Model
+{
+    use HasFactory;
+}
